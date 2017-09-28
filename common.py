13ac6cc8ccbf200fd17727aa3290d0beee845dd9
IMG-PRCSNG <samthedevil.sp@gmail.com>
2017-09-28 22:39:58 +0530

    Train any pytorch model on any dataset (Except for 1000 clasess) :P

---
+++ common.py
@@ -0,0 +1,33 @@
+# Copied from 
+model_urls = {
+    'alexnet': 'https://download.pytorch.org/models/alexnet-owt-4df8aa71.pth',
+    'densenet121': 'https://download.pytorch.org/models/densenet121-241335ed.pth',
+    'densenet169': 'https://download.pytorch.org/models/densenet169-6f0f7f60.pth',
+    'densenet201': 'https://download.pytorch.org/models/densenet201-4c113574.pth',
+    'densenet161': 'https://download.pytorch.org/models/densenet161-17b70270.pth',
+    #truncated _google to match module name
+    'inception_v3': 'https://download.pytorch.org/models/inception_v3_google-1a9a5a14.pth',
+    'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
+    'resnet34': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
+    'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
+    'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
+    'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',    
+    'squeezenet1_0': 'https://download.pytorch.org/models/squeezenet1_0-a815701f.pth',
+    'squeezenet1_1': 'https://download.pytorch.org/models/squeezenet1_1-f364aa15.pth',
+    'vgg11': 'https://download.pytorch.org/models/vgg11-bbd30ac9.pth',
+    'vgg13': 'https://download.pytorch.org/models/vgg13-c768596a.pth',
+    'vgg16': 'https://download.pytorch.org/models/vgg16-397923af.pth',
+    'vgg19': 'https://download.pytorch.org/models/vgg19-dcbb9e9d.pth',    
+}
+
+model_names = model_urls.keys()
+
+input_sizes = {
+    'alexnet' : 224,
+    'densenet': 224,
+    'resnet' : 224,
+    'inception' : 299,
+    'squeezenet' : 224,#not 255,255 acc. to https://github.com/pytorch/pytorch/issues/1120
+    'vgg' : 224
+}
+
